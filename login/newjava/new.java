1ed7ceb6f9edc83bcee81eeda3887487f6f47913
VishvPruthi <vishvpruthi@gmail.com>
2025-06-08 22:32:02 +0530

    Add files via upload

---
+++ login/newjava/new.java
@@ -0,0 +1,17 @@
+class A {
+    public void display() {
+       System.out.println("A");
+    }
+ }
+ class B extends A {
+    public void area() {
+       System.out.println("B");
+    }
+ }
+class main {
+    public static void main(String[] args) {
+       NEW new = new new();
+       new.display();
+       new.area();
+    }
+ }
